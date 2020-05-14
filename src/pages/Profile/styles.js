39988aec30d0534587fe0525ab65d2d17b2ec4ec
Guilhermo Masid <Guilhermo@MacBook-Air-de-Guilhermo.local>
2020-05-14 16:36:46 -0300

    Aula 17 - Atualizacao de perfil

---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(0, 0, 0, 0.1);
+  margin: 20px 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`;
+
+export const Form = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { padding: 30 },
+})`
+  align-self: stretch;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 10px;
+`;
